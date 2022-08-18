45729298a4736c963b70aacee84c6b9464470a39
sampu3011 <53847721+sampu3011@users.noreply.github.com>
2022-08-18 16:27:49 +0530

    Update app.js
    version3 updated

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app.' + '\n';
+    response = 'Vanakkam version 3 app.' + '\n';
 
     //send the response to the client
     res.send(response);
